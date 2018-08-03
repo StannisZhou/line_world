044ac092a81c32cbc9d2d6e9f65b21bdacf9fb7a
Guangyao Zhou <tczhouguangyao@gmail.com>
2018-08-03 03:21:37 -0400

    Factory pattern for cycles perturbation

--- line_world/cycles_machine.py
+++ line_world/cycles_machine.py
@@ -1,7 +1,7 @@
 from line_world.utils import ParamsProc, Component, Optional
 import numpy as np
 from line_world.layer import Layer
-from line_world.perturbation import log_prob_cycles_perturbation, draw_samples_markov_backbone
+from line_world.perturbation import create_cycles_perturbation
 import torch
 
 
@@ -12,6 +12,21 @@ class CyclesMachine(Component):
         proc.add(
             'layer_params_list', list,
             'The parameters for the different layers in the Markov backbone'
+        )
+        proc.add(
+            'cycles_perturbation_implementation', str,
+            'The cycles perturbation implementation we are going to use',
+            'markov_backbone'
+        )
+        proc.add(
+            'cycles_perturbation_params', dict,
+            'The parameters for the cycles perturbation',
+            {}
+        )
+        proc.add(
+            'n_samples', int,
+            'The number of samples we are going to use to estimate the null distribution for the number of cycles',
+            0
         )
         return proc
 
@@ -36,9 +51,16 @@ class CyclesMachine(Component):
         for layer_params in self.params['layer_params_list']:
             self.layer_list.append(Layer(layer_params))
 
+        self.cycles_perturbation = create_cycles_perturbation(
+            self.params['cycles_perturbation_implementation'],
+            self.layer_list, self.params['n_samples'],
+            self.params['cycles_perturbation_params']
+        )
+
     def get_energy(self, state_list):
         log_prob = log_prob_markov_backbone(state_list, self.layer_list) + \
-            log_prob_cycles_perturbation(state_list, self.layer_list)
+            self.cycles_perturbation.get_log_prob_cycles_perturbation(state_list)
+
         return log_prob
 
     def evaluate_energy_gradients(self, state_list):
--- line_world/perturbation.py
+++ line_world/perturbation.py
@@ -1,13 +1,29 @@
 from line_world.utils import ParamsProc, Component
 from tqdm import tqdm
 import torch
-
-
-def log_prob_cycles_perturbation(state_list, layer_list):
-    return 0
+import logging
 
 
 def get_n_cycles(state_list, layer_list):
+    """get_n_cycles
+    Get the number of cycles for different layers at a given state
+
+    Parameters
+    ----------
+
+    state_list : list
+        state_list is a list of torch tensors representing the states of different layers
+    layer_list : list
+        layer_list is a list of Layer objects representing the different layers in the model
+
+    Returns
+    -------
+
+    n_cycles_list : list
+        A list of length n_layers - 2 containing the number of cycles at each brick.
+        Each element is a tensor of the same shape as the corresponding layer
+
+    """
     assert len(state_list) == len(layer_list)
     n_cycles_list = [
         get_n_cycles_three_layers(state_list[ii:ii + 3], layer_list[ii:ii + 3])
@@ -17,6 +33,25 @@ def get_n_cycles(state_list, layer_list):
 
 
 def get_n_cycles_three_layers(state_list, layer_list):
+    """get_n_cycles_three_layers
+    Function for getting the number of cycles for three layers
+
+    Parameters
+    ----------
+
+    state_list : list
+        state_list is a list of length 3
+    layer_list : list
+        layer_list is a list of length 3
+
+    Returns
+    -------
+
+    n_cycles : torch.Tensor
+        n_cycles is a tensor of the same size as the top layer, containing the number of
+        cycles for each brick in the top layer
+
+    """
     assert len(state_list) == 3
     assert len(layer_list) == 3
     on_bricks_prob_list = [layer_list[ii].get_on_bricks_prob(state_list[ii]) for ii in range(3)]
@@ -36,6 +71,23 @@ def get_n_cycles_three_layers(state_list, layer_list):
 
 
 def draw_samples_markov_backbone(layer_list):
+    """draw_samples_markov_backbone
+    Draw samples from the Markov backbone
+
+    Parameters
+    ----------
+
+    layer_list : list
+        layer_list is a list of layers
+
+    Returns
+    -------
+
+    layer_sample_list : list
+        A list of samples for different layer. Each element is a tensor of state_shape of
+        each layer
+
+    """
     layer_sample_list = []
     no_parents_prob = torch.ones(layer_list[0].shape)
     for layer in layer_list[:-1]:
@@ -47,11 +99,70 @@ def draw_samples_markov_backbone(layer_list):
     return layer_sample_list
 
 
-def get_n_cycles_null_distribution(state_list, layer_list, n_samples):
-    state_list_samples = [
-        draw_samples_markov_backbone(layer_list) for _ in tqdm(range(n_samples))
-    ]
-    n_cycles_statistics = [
-        get_n_cycles(state_list, layer_list) for state_list in state_list_samples
-    ]
-    return n_cycles_statistics
+def create_cycles_perturbation(implementation, layer_list, n_samples, params):
+    """create_cycles_perturbation
+    Factory method for creating cycles perturbation
+
+    Parameters
+    ----------
+
+    implementation : str
+        implementation is the implementation we are going to use for the perturbation.
+        Supported implementations include:
+            markov_backbone : no perturbation
+            toy_perturbation : a simple perturbation for the toy case where the top layer
+            has only one brick
+    layer_list : list
+        layer_list is a list of layers in the model
+    n_samples : int
+        n_samples is the number of cycles we are going to use to estimate the null distribution
+        on the number of cycles
+    params : dict
+        params is a dictionary containing the various parameters for the cycles perturbation
+
+    Returns
+    -------
+
+    A class for cycles perturbation
+
+    """
+    if implementation == 'markov_backbone':
+        return MarkovBackbone(layer_list, n_samples, params)
+    elif implementation == 'toy_perturbation':
+        return ToyPerturbation(layer_list, n_samples, params)
+    else:
+        raise Except('Unsupported cycles perturbation implementation')
+
+
+class CyclesPerturbation(object):
+    def __init__(self, layer_list, n_samples):
+        self.layer_list = layer_lists
+        logging.info('Getting samples for the null distribution on the number of cycles')
+        state_list_samples = [
+            draw_samples_markov_backbone(layer_list) for _ in tqdm(range(n_samples))
+        ]
+        self.n_cycles_statistics = [
+            get_n_cycles(state_list, layer_list) for state_list in state_list_samples
+        ]
+
+    def get_log_prob_cycles_perturbation(self, state_list):
+        raise Exception("Must be implemented")
+
+
+class MarkovBackbone(CyclesPerturbation):
+    def __init__(self, layer_list, n_samples, params):
+        pass
+
+    def get_log_prob_cycles_perturbation(self, state_list):
+        return 0
+
+
+class ToyPerturbation(CyclesPerturbation):
+    def __init__(self, layer_list, n_samples, params):
+        assert len(layer_list) == 3
+        assert layer_list[0].shape == torch.Size([1, 1, 1])
+        super().__init__(layer_list, n_samples)
+        self.params = params
+
+    def get_log_prob_cycles_perturbation(self, state_list):
+        return 0

8da10a539bcb5ee6b7c68f0945d98416030f5c86
Guangyao Zhou <tczhouguangyao@gmail.com>
2018-08-24 12:23:42 -0400

    Use new toy module in scripts

--- scripts/toy_problem_three_parts.py
+++ scripts/toy_problem_three_parts.py
@@ -2,79 +2,20 @@ import numpy as np
 from line_world.cycles_machine import CyclesMachine
 from line_world.params import generate_cycles_machine_layer_params
 from line_world.sample.markov_backbone import draw_sample_markov_backbone
+from line_world.toy.model import three_parts
 import torch
 import torch.optim
 
-# Set basic parameters
-n_layers = 3
-n_channels_list = np.ones(n_layers - 1, dtype=int)
-d_image = 6
-kernel_size_list = np.array([4, 3], dtype=int)
-stride_list = np.ones(n_layers - 1, dtype=int)
 self_rooting_prob_list = np.array([1.0, 0.01, 0.01])
-thickness = 1
-length = 3
-n_rotations = 4
-n_parts = 3
-order = 0
-cycles_perturbation_implementation = 'toy_perturbation'
 perturbed_distribution = 0.0001 * torch.ones(6, dtype=torch.float)
 perturbed_distribution[5] = 0.9995
-cycles_perturbation_params = {
-    'perturbed_distribution': perturbed_distribution,
-    'sigma': 1.0
-}
+sigma = 1.0
 n_samples = int(3e3)
 
-# Initialize the CyclesMachine
-layer_params_list = generate_cycles_machine_layer_params(
-    n_layers, n_channels_list, d_image, kernel_size_list, stride_list, self_rooting_prob_list,
-    thickness, length, n_rotations, n_parts, order
+cycles_machine, optimal_state_list, optimal_log_prob = three_parts(
+    self_rooting_prob_list, perturbed_distribution, sigma, n_samples
 )
-cycles_machine = CyclesMachine({
-    'layer_params_list': layer_params_list,
-    'cycles_perturbation_implementation': cycles_perturbation_implementation,
-    'cycles_perturbation_params': cycles_perturbation_params,
-    'n_samples': n_samples,
-})
-
-image = torch.zeros(cycles_machine.layer_list[2].state_shape)
-for ii in range(6):
-    for jj in range(6):
-        if ii == jj:
-            if ii == 0:
-                image[0, ii, jj, 0] = 1
-            else:
-                image[0, ii, jj, 1] = 1
-        else:
-            image[0, ii, jj, 0] = 1
-
-expanded_templates = cycles_machine.layer_list[0].expanded_templates.to_dense().numpy()
-for ind in range(expanded_templates.shape[3]):
-    if np.all(expanded_templates[0, 0, 0, ind] == np.array([[
-        [0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]
-    ]])):
-        break
-
-optimal_top_layer = torch.zeros(cycles_machine.layer_list[0].state_shape)
-optimal_top_layer[0, 0, 0, ind] = 1
-optimal_middle_layer = torch.zeros(cycles_machine.layer_list[1].state_shape)
-for ii in range(4):
-    for jj in range(4):
-        if ii == jj:
-            if ii == 0:
-                optimal_middle_layer[0, ii, jj, 0] = 1
-            else:
-                optimal_middle_layer[0, ii, jj, 8] = 1
-        else:
-            optimal_middle_layer[0, ii, jj, 0] = 1
-
-optimal_state_list = [
-    optimal_top_layer,
-    optimal_middle_layer,
-    image
-]
-optimal_log_prob = cycles_machine.get_energy(optimal_state_list)
+image = optimal_state_list[-1]
 state_list = [
     torch.rand(cycles_machine.layer_list[0].state_shape).requires_grad_(),
     torch.rand(cycles_machine.layer_list[1].state_shape).requires_grad_(),
--- scripts/toy_problem_two_parts.py
+++ scripts/toy_problem_two_parts.py
@@ -1,62 +1,20 @@
 import numpy as np
-from line_world.cycles_machine import CyclesMachine
-from line_world.params import generate_cycles_machine_layer_params
-from line_world.sample.markov_backbone import draw_sample_markov_backbone
 import torch
 import torch.optim
+from line_world.toy.model import two_parts
 
-# Set basic parameters
-n_layers = 3
-n_channels_list = np.ones(n_layers - 1, dtype=int)
-d_image = 4
-kernel_size_list = np.array([2, 3], dtype=int)
-stride_list = np.ones(n_layers - 1, dtype=int)
+
 self_rooting_prob_list = np.array([0.5, 0.001, 0.001])
-thickness = 1
-length = 3
-n_rotations = 4
-n_parts = 2
-order = 0
-cycles_perturbation_implementation = 'toy_perturbation'
-cycles_perturbation_params = {
-    'perturbed_distribution': torch.tensor([0.01, 0.01, 0.97, 0.01]),
-    'sigma': 0.2
-}
+perturbed_distribution = torch.tensor([0.01, 0.01, 0.97, 0.01])
+sigma = 0.2
 n_samples = int(2e3)
 
-# Initialize the CyclesMachine
-layer_params_list = generate_cycles_machine_layer_params(
-    n_layers, n_channels_list, d_image, kernel_size_list, stride_list, self_rooting_prob_list,
-    thickness, length, n_rotations, n_parts, order
+cycles_machine, optimal_state_list, optimal_log_prob = two_parts(
+    self_rooting_prob_list, perturbed_distribution, sigma, n_samples
 )
-cycles_machine = CyclesMachine({
-    'layer_params_list': layer_params_list,
-    'cycles_perturbation_implementation': cycles_perturbation_implementation,
-    'cycles_perturbation_params': cycles_perturbation_params,
-    'n_samples': n_samples
-})
 
-image = torch.zeros(cycles_machine.layer_list[2].state_shape)
-for ii in range(4):
-    for jj in range(4):
-        if ii == jj:
-            image[0, ii, jj, 1] = 1
-        else:
-            image[0, ii, jj, 0] = 1
+image = optimal_state_list[-1]
 
-optimal_top_layer = torch.zeros(cycles_machine.layer_list[0].state_shape)
-optimal_top_layer[0, 0, 0, 3] = 1
-optimal_middle_layer = torch.zeros(cycles_machine.layer_list[1].state_shape)
-optimal_middle_layer[0, 0, 0, 8] = 1
-optimal_middle_layer[0, 0, 1, 0] = 1
-optimal_middle_layer[0, 1, 0, 0] = 1
-optimal_middle_layer[0, 1, 1, 8] = 1
-optimal_state_list = [
-    optimal_top_layer,
-    optimal_middle_layer,
-    image
-]
-optimal_log_prob = cycles_machine.get_energy(optimal_state_list)
 state_list = [
     torch.rand(cycles_machine.layer_list[0].state_shape).requires_grad_(),
     torch.rand(cycles_machine.layer_list[1].state_shape).requires_grad_(),
